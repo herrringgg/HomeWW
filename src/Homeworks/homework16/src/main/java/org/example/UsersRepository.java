6add786196a28113e88db9d8316024916967a0c0
herrringgg <gavrilovaann2@gmail.com>
2025-09-24 21:39:44 +0500

    homework16

---
+++ src/Homeworks/homework16/src/main/java/org/example/UsersRepository.java
@@ -0,0 +1,12 @@
+package org.example;
+
+import java.util.List;
+
+public interface UsersRepository {
+    void create(User user);
+    User findById(String id);
+    List<User> findAll();
+    void update(User user);
+    void deleteById(String id);
+    void deleteAll();
+}
